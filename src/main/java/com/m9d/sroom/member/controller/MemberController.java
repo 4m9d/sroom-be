50e43d281366c5d1a6dfdff23e8bef196edd92af
정두원 <jdu2121@ajou.ac.kr>
2023-07-03 10:41:28 +0900

    [SWM-136] Docs : add swagger annotation login

--- src/main/java/com/m9d/sroom/member/controller/MemberController.java
+++ src/main/java/com/m9d/sroom/member/controller/MemberController.java
@@ -1,10 +1,20 @@
 package com.m9d.sroom.member.controller;
+
 import com.m9d.sroom.member.dto.request.GoogleIdKey;
 import com.m9d.sroom.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +25,15 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody GoogleIdKey request) throws Exception{
-        return ResponseEntity.ok(memberService.authenticateUser(request.getClientId(), request.getGoogleIdKey()));
+    @Tag(name = "로그인")
+    @Operation(summary = "사용자 로그인", description = "구글 ID 키를 사용하여 사용자를 인증합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 사용자를 인증하였습니다.", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "요청 본문이 유효하지 않습니다.", content = @Content),
+            @ApiResponse(responseCode = "401", description = "인증에 실패하였습니다.", content = @Content)
+    })
+    public ResponseEntity<?> login(
+            @RequestBody(description = "구글 ID 토큰", required = true, content = @Content(schema = @Schema(implementation = GoogleIdKey.class))) @Valid GoogleIdKey googleIdKey) throws Exception {
+        return ResponseEntity.ok(memberService.authenticateMember(googleIdKey.getGoogleIdKey()));
     }
 }
