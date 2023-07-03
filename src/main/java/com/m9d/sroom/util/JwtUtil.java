074743e6d9216109b24ccd3c471819a7fc193324
정두원 <jdu2121@ajou.ac.kr>
2023-07-03 14:39:20 +0900

    [SWM-136] Fix : login response dto change

--- src/main/java/com/m9d/sroom/util/JwtUtil.java
+++ src/main/java/com/m9d/sroom/util/JwtUtil.java
@@ -1,12 +1,15 @@
 package com.m9d.sroom.util;
 
 import com.m9d.sroom.member.domain.Member;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class JwtUtil {
@@ -18,7 +21,7 @@ public class JwtUtil {
         return Jwts.builder()
                 .setSubject(member.getMemberCode())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10시간 유효 토큰
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256, jwtSecret)
                 .compact();
     }
@@ -29,5 +32,14 @@ public class JwtUtil {
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7)) // 7일 유효 토큰
                 .signWith(SignatureAlgorithm.HS256, jwtSecret).compact();
+    }
+
+    public Long getExpirationTimeFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getExpiration().getTime() / 1000; // convert to Unix time
     }
 }
