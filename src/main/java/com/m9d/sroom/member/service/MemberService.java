5f5c7f0629fc13a6f4a1cc9c35f657d3bbd90ca4
정두원 <jdu2121@ajou.ac.kr>
2023-07-04 16:56:12 +0900

    [SWM-138] Refactor : Decompose authenticate method into smaller methods

--- src/main/java/com/m9d/sroom/member/service/MemberService.java
+++ src/main/java/com/m9d/sroom/member/service/MemberService.java
@@ -6,13 +6,11 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier.Builde
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.json.webtoken.JsonWebToken;
 import com.m9d.sroom.member.domain.Member;
 import com.m9d.sroom.member.dto.response.Login;
 import com.m9d.sroom.member.exception.CredentialUnauthorizedException;
 import com.m9d.sroom.member.repository.MemberRepository;
 import com.m9d.sroom.util.JwtUtil;
-import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -33,31 +31,10 @@ public class MemberService {
 
     @Transactional
     public Login authenticateMember(String credential) throws Exception {
-        HttpTransport transport = new NetHttpTransport();
-        JacksonFactory jsonFactory = new JacksonFactory();
+        GoogleIdToken.Payload payload = getPayloadFromCredential(credential);
+        String memberCode = getMemberCodeFromPayload(payload);
 
-        GoogleIdTokenVerifier verifier = new Builder(transport, jsonFactory)
-                .setAudience(Collections.singletonList(clientId))
-                .build();
-        GoogleIdToken idToken = verifier.verify(credential);
-        GoogleIdToken.Payload payload;
-
-        try {
-            payload = idToken.getPayload();
-        } catch (NullPointerException e) {
-            throw new CredentialUnauthorizedException();
-        }
-        String memberCode = payload.getSubject();
-        Optional<Member> memberOptional = memberRepository.findByMemberCode(memberCode);
-        Member member;
-
-        if (memberOptional.isPresent()) {
-            member = memberOptional.get();
-        } else {
-            String memberName = generateMemberName();
-            memberRepository.save(memberCode, memberName);
-            member = memberRepository.getByMemberCode(memberCode);
-        }
+        Member member = findOrCreateMember(memberCode);
 
         String accessToken = jwtUtil.generateAccessToken(member);
         String refreshToken = jwtUtil.generateRefreshToken(member);
@@ -75,6 +52,42 @@ public class MemberService {
         return login;
     }
 
+    @Transactional
+    public GoogleIdToken.Payload getPayloadFromCredential(String credential) throws Exception {
+        HttpTransport transport = new NetHttpTransport();
+        JacksonFactory jsonFactory = new JacksonFactory();
+
+        GoogleIdTokenVerifier verifier = new Builder(transport, jsonFactory)
+                .setAudience(Collections.singletonList(clientId))
+                .build();
+        GoogleIdToken idToken = verifier.verify(credential);
+
+        if (idToken == null) {
+            throw new CredentialUnauthorizedException();
+        }
+
+        return idToken.getPayload();
+    }
+
+    @Transactional
+    public String getMemberCodeFromPayload(GoogleIdToken.Payload payload) {
+        return payload.getSubject();
+    }
+
+    @Transactional
+    public Member findOrCreateMember(String memberCode) {
+        return memberRepository.findByMemberCode(memberCode)
+                .orElseGet(() -> createNewMember(memberCode));
+    }
+
+    @Transactional
+    public Member createNewMember(String memberCode) {
+        String memberName = generateMemberName();
+        memberRepository.save(memberCode, memberName);
+        return memberRepository.getByMemberCode(memberCode);
+    }
+
+    @Transactional
     public String generateMemberName() {
         final Random RANDOM = new Random();
         final int NUMBER_BOUND = 1000000;
