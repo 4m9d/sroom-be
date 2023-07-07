871aec4db1a82874e80299eb060c15a406f43fa2
정두원 <jdu2121@ajou.ac.kr>
2023-07-07 19:16:16 +0900

    [SWM-139] Test : add case keyword search controller

--- src/test/java/com/m9d/sroom/util/ControllerTest.java
+++ src/test/java/com/m9d/sroom/util/ControllerTest.java
@@ -1,12 +1,22 @@
 package com.m9d.sroom.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.m9d.sroom.lecture.dto.response.KeywordSearch;
+import com.m9d.sroom.member.domain.Member;
+import com.m9d.sroom.member.dto.response.Login;
 import com.m9d.sroom.member.repository.MemberRepository;
 import com.m9d.sroom.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,5 +33,32 @@ public class ControllerTest {
 
     @Autowired
     protected MemberRepository memberRepository;
+
+    public Login getNewLogin() {
+        Member member = getNewMember();
+        Login login = memberService.generateLogin(member);
+        return login;
+    }
+
+    protected Member getNewMember() {
+        GoogleIdToken.Payload payload = new GoogleIdToken.Payload();
+        String expectedMemberCode = "106400356559989163499";
+        String memberCode = memberService.getMemberCodeFromPayload(payload.setSubject(expectedMemberCode));
+
+        return memberService.findOrCreateMember(memberCode);
+    }
+
+    protected KeywordSearch getKeywordSearch(Login login, String keyword, int limit) throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get("/lectures")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", login.getAccessToken())
+                        .queryParam("keyword", keyword)
+                        .queryParam("limit", String.valueOf(limit)))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+
+        String jsonContent = response.getContentAsString();
+        return objectMapper.readValue(jsonContent, KeywordSearch.class);
+    }
 
 }
