757fcccf971e777a80d8cb7bcee95079fc6a43f0
정두원 <jdu2121@ajou.ac.kr>
2023-07-14 18:36:07 +0900

    [#25] Fix : duration time format fix

--- src/main/java/com/m9d/sroom/lecture/service/LectureService.java
+++ src/main/java/com/m9d/sroom/lecture/service/LectureService.java
@@ -301,7 +301,7 @@ public class LectureService {
         if (hours > 0) {
             return String.format("%d:%02d:%02d", hours, minutes, seconds);
         } else {
-            return String.format("%02d:%02d", minutes, seconds);
+            return String.format("%d:%02d", minutes, seconds);
         }
     }
 
