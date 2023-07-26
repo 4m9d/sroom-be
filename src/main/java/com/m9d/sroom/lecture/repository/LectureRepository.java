b491eae0a3e9c4f8122d80569f1e3d3e75f04a53
정두원 <jdu2121@ajou.ac.kr>
2023-07-26 13:43:02 +0900

    [SWM-188] Refactor : use groovy to sql

--- src/main/java/com/m9d/sroom/lecture/repository/LectureRepository.java
+++ src/main/java/com/m9d/sroom/lecture/repository/LectureRepository.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Repository;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-import static com.m9d.sroom.lecture.sql.LectureSqlQuery.*;
-
 @Repository
 public class LectureRepository {
     private final JdbcTemplate jdbcTemplate;
@@ -23,7 +21,7 @@ public class LectureRepository {
     public List<ReviewBrief> getReviewBriefList(String lectureCode, int reviewOffset, int reviewLimit) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-        List<ReviewBrief> reviewBriefList = jdbcTemplate.query(GET_REVIEW_BRIEF_LIST_QUERY,
+        return jdbcTemplate.query(LectureSqlQuery.GET_REVIEW_BRIEF_LIST_QUERY,
                 (rs, rowNum) -> ReviewBrief.builder()
                         .index(rowNum + reviewOffset + 1)
                         .reviewContent(rs.getString("content"))
@@ -32,20 +30,14 @@ public class LectureRepository {
                         .publishedAt(dateFormat.format(rs.getTimestamp("submitted_date")))
                         .build(),
                 lectureCode, reviewLimit, reviewOffset);
-
-        return reviewBriefList;
     }
 
 
     public HashSet<String> getVideosByMemberId(Long memberId) {
-        Set<String> videoSet = new HashSet<>(jdbcTemplate.query(GET_VIDEOS_BY_MEMBER_ID_QUERY, (rs, rowNum) -> rs.getString("video_code"), memberId));
-
-        return new HashSet<>(videoSet);
+        return new HashSet<>(jdbcTemplate.query(LectureSqlQuery.GET_VIDEOS_BY_MEMBER_ID_QUERY, (rs, rowNum) -> rs.getString("video_code"), memberId));
     }
 
     public HashSet<String> getPlaylistByMemberId(Long memberId) {
-        Set<String> playlistSet = new HashSet<>(jdbcTemplate.query(GET_PLAYLIST_BY_MEMBER_ID_QUERY, (rs, rowNum) -> rs.getString("playlist_code"), memberId));
-
-        return new HashSet<>(playlistSet);
+        return new HashSet<>(jdbcTemplate.query(LectureSqlQuery.GET_PLAYLIST_BY_MEMBER_ID_QUERY, (rs, rowNum) -> rs.getString("playlist_code"), memberId));
     }
 }
--- src/main/java/com/m9d/sroom/member/repository/MemberRepository.java
+++ src/main/java/com/m9d/sroom/member/repository/MemberRepository.java
@@ -2,18 +2,14 @@ package com.m9d.sroom.member.repository;
 
 import com.m9d.sroom.member.domain.Member;
 import com.m9d.sroom.member.exception.MemberNotFoundException;
-import lombok.RequiredArgsConstructor;
+import com.m9d.sroom.member.sql.MemberSqlQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
-
-import static com.m9d.sroom.member.sql.MemberSqlQuery.*;
 
 @Repository
 public class MemberRepository {
@@ -26,25 +22,29 @@ public class MemberRepository {
     }
 
     public void save(String memberCode, String memberName) {
-        jdbcTemplate.update(SAVE_MEMBER_QUERY, memberCode, memberName);
+        jdbcTemplate.update(MemberSqlQuery.SAVE_MEMBER_QUERY, memberCode, memberName);
     }
 
     public Member getByMemberCode(String memberCode) {
-        try {
-            Member member = jdbcTemplate.queryForObject(GET_BY_MEMBER_CODE_QUERY, memberRowMapper, memberCode);
-            return member;
-        } catch (EmptyResultDataAccessException e) {
-            throw new MemberNotFoundException();
-        }
+        return jdbcTemplate.queryForObject(MemberSqlQuery.GET_BY_MEMBER_CODE_QUERY, memberRowMapper, memberCode);
     }
 
     public Optional<Member> findByMemberCode(String memberCode) {
-        try {
-            Member member = jdbcTemplate.queryForObject(FIND_BY_MEMBER_CODE_QUERY, memberRowMapper, memberCode);
-            return Optional.ofNullable(member);
-        } catch (EmptyResultDataAccessException e) {
-            return Optional.empty();
-        }
+        Member member = queryForObjectOrNull(MemberSqlQuery.FIND_BY_MEMBER_CODE_QUERY, memberRowMapper, memberCode);
+        return Optional.ofNullable(member);
+    }
+
+    public void saveRefreshToken(Long memberId, String refreshToken) {
+        jdbcTemplate.update(MemberSqlQuery.SAVE_REFRESH_TOKEN_QUERY, refreshToken, memberId);
+    }
+
+    public Optional<Member> findByMemberId(Long memberId) {
+        Member member = queryForObjectOrNull(MemberSqlQuery.FIND_BY_MEMBER_ID_QUERY, memberRowMapper, memberId);
+        return Optional.ofNullable(member);
+    }
+
+    public String getRefreshById(Long memberId) {
+        return jdbcTemplate.queryForObject(MemberSqlQuery.GET_REFRESH_BY_ID_QUERY, (rs, rowNum) -> rs.getString(1), memberId);
     }
 
     private final RowMapper<Member> memberRowMapper = (rs, rowNum) -> Member.builder()
@@ -54,25 +54,11 @@ public class MemberRepository {
             .bio(rs.getString("bio"))
             .build();
 
-    public void saveRefreshToken(Long memberId, String refreshToken) {
-        jdbcTemplate.update(SAVE_REFRESH_TOKEN_QUERY, refreshToken, memberId);
-    }
-
-    public Optional<Member> findByMemberId(Long memberId) {
+    private <T> T queryForObjectOrNull(String sql, RowMapper<T> rowMapper, Object... args) {
         try {
-            Member member = jdbcTemplate.queryForObject(FIND_BY_MEMBER_ID_QUERY, memberRowMapper, memberId);
-            return Optional.ofNullable(member);
-        } catch (IncorrectResultSizeDataAccessException e) {
-            return Optional.empty();
-        }
-    }
-
-    public String getRefreshById(Long memberId) {
-        try {
-            String refreshToken = jdbcTemplate.queryForObject(GET_REFRESH_BY_ID_QUERY, (rs, rowNum) -> rs.getString(1), memberId);
-            return refreshToken;
+            return jdbcTemplate.queryForObject(sql, rowMapper, args);
         } catch (EmptyResultDataAccessException e) {
-            throw new MemberNotFoundException();
+            return null;
         }
     }
 }
