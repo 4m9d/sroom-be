d1b35f1ac9f35455d3fd55e03bdb9eeb058114f6
GyeongSik Son <47748085+Son-GyeongSik@users.noreply.github.com>
2023-07-31 16:37:51 +0900

    [SWM-166] Test : modify test code for my courses

--- src/test/java/com/m9d/sroom/course/service/CourseServiceTest.java
+++ src/test/java/com/m9d/sroom/course/service/CourseServiceTest.java
@@ -2,6 +2,7 @@ package com.m9d.sroom.course.service;
 
 import com.m9d.sroom.course.dto.response.CourseInfo;
 import com.m9d.sroom.course.dto.response.EnrolledCourseInfo;
+import com.m9d.sroom.course.dto.response.MyCourses;
 import com.m9d.sroom.member.domain.Member;
 import com.m9d.sroom.util.ServiceTest;
 import org.junit.jupiter.api.Test;
@@ -18,26 +19,33 @@ public class CourseServiceTest extends ServiceTest {
     @DisplayName("유저가 등록한 강의 코스들의 정보를 불러옵니다.")
     void getCourseListTest() {
         //given
-        Long memberId = 1L;
+        Member member = getNewMember();
+        Long memberId = member.getMemberId();
 
-        String courseInsertSql = "INSERT INTO COURSE(member_id, course_title, thumbnail) values(?, ?, ?)";
-        jdbcTemplate.update(courseInsertSql, memberId, "course1", " ");
-        jdbcTemplate.update(courseInsertSql, memberId, "course2", " ");
-        jdbcTemplate.update(courseInsertSql, memberId, "course3", " ");
+        String courseInsertSql = "INSERT INTO COURSE(member_id, course_title, thumbnail, last_view_time, progress) values(?, ?, ?, ?, ?)";
+        jdbcTemplate.update(courseInsertSql, memberId, "course1", " ", "2023-06-13", 100);
+        jdbcTemplate.update(courseInsertSql, memberId, "course2", " ", "2023-04-14", 93);
+        jdbcTemplate.update(courseInsertSql, memberId, "course3", " ", "2023-11-21", 40);
 
         String lectureInsertSql = "INSERT INTO LECTURE(course_id, channel, is_playlist) values(?, ?, ?)";
         String getCourseIdSql = "SELECT course_id FROM COURSE WHERE course_title = ?";
+        String courseVideoInsertSql = "INSERT INTO COURSEVIDEO(course_id, is_complete, video_id, summary_id, video_index) values(?, ?, 0, 0, 0)";
         for (int i = 1; i <= 3; i++) {
             String courseId = jdbcTemplate.queryForObject(getCourseIdSql, String.class, "course".concat(String.valueOf(i)));
-            jdbcTemplate.update(lectureInsertSql, Integer.valueOf(courseId), "channel1", "false");
-            jdbcTemplate.update(lectureInsertSql, Integer.valueOf(courseId), "channel2", "false");
+            jdbcTemplate.update(lectureInsertSql, Long.valueOf(courseId), "channel1", "false");
+            jdbcTemplate.update(lectureInsertSql, Long.valueOf(courseId), "channel2", "false");
+            jdbcTemplate.update(courseVideoInsertSql, Long.valueOf(courseId), true);
+            jdbcTemplate.update(courseVideoInsertSql, Long.valueOf(courseId), false);
         }
 
+
+
+
         //when
-        List<CourseInfo> courseList = courseService.getCourseList(memberId);
+        MyCourses myCourses = courseService.getMyCourses(memberId);
 
         //then
-        Assertions.assertEquals(3, courseList.size());
+        System.out.println(myCourses);
     }
 
 
