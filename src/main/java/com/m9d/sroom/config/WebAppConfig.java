9fadfef355f31553bf410dc2319029de872f878f
정두원 <jdu2121@ajou.ac.kr>
2023-08-08 21:59:29 +0900

    Feat : add CORS mapping for request localhost:3000

--- src/main/java/com/m9d/sroom/config/WebAppConfig.java
+++ src/main/java/com/m9d/sroom/config/WebAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -41,4 +42,11 @@ public class WebAppConfig implements WebMvcConfigurer {
         return new Gson();
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowCredentials(true)
+                .exposedHeaders("authorization");
+    }
 }
