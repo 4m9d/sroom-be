79f55f2b5ab933dbed1ec64c33f09dd72822ad50
정두원 <jdu2121@ajou.ac.kr>
2024-03-01 01:07:00 +0900

    [SWM-426] Refactor : use ehcache in case getting video or playlist info

--- src/main/java/com/m9d/sroom/common/EhcacheController.java
+++ src/main/java/com/m9d/sroom/common/EhcacheController.java
@@ -1,10 +1,13 @@
 package com.m9d.sroom.common;
 
+import com.m9d.sroom.playlist.PlaylistService;
+import com.m9d.sroom.video.VideoService;
 import net.sf.ehcache.Ehcache;
 import net.sf.ehcache.Element;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.ehcache.EhCacheCache;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +19,12 @@ import java.util.stream.Collectors;
 public class EhcacheController {
 
     private final CacheManager cacheManager;
-
-    public EhcacheController(CacheManager cacheManager) {
+    private final VideoService videoService;
+    private final PlaylistService playlistService;
+    public EhcacheController(CacheManager cacheManager, VideoService videoService, PlaylistService playlistService) {
         this.cacheManager = cacheManager;
+        this.videoService = videoService;
+        this.playlistService = playlistService;
     }
 
     @GetMapping("/ehcache")
@@ -35,9 +41,18 @@ public class EhcacheController {
                             entry.computeIfAbsent(cacheName, k -> new ArrayList<>()).add(element.toString());
                         }
                     });
-
                     return entry;
                 })
                 .collect(Collectors.toList());
     }
+
+//    @GetMapping("/ehcache/test/video/{videoCode}")
+//    public Object getVideoInfo(@PathVariable String videoCode){
+//        return videoService.getRecentVideo(videoCode);
+//    }
+//
+//    @GetMapping("/ehcache/test/playlist/{playlistCode}")
+//    public Object getPlaylistInfo(@PathVariable String playlistCode){
+//        return playlistService.getRecentPlaylist(playlistCode);
+//    }
 }
