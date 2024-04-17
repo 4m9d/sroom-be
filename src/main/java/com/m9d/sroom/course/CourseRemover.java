f612b21afb1c025b7b32625d92ae5611190c418f
정두원 <jdu2121@ajou.ac.kr>
2024-04-17 23:55:51 +0900

    Fix : make lecture id null in review entity if lecture is removed

--- src/main/java/com/m9d/sroom/course/CourseRemover.java
+++ src/main/java/com/m9d/sroom/course/CourseRemover.java
@@ -1,12 +1,17 @@
 package com.m9d.sroom.course;
 
 import com.m9d.sroom.common.entity.jpa.CourseEntity;
+import com.m9d.sroom.common.entity.jpa.LectureEntity;
+import com.m9d.sroom.common.entity.jpa.ReviewEntity;
 import com.m9d.sroom.common.repository.course.CourseJpaRepository;
 import com.m9d.sroom.common.repository.coursequiz.CourseQuizJpaRepository;
 import com.m9d.sroom.common.repository.coursevideo.CourseVideoJpaRepository;
 import com.m9d.sroom.common.repository.lecture.LectureJpaRepository;
+import com.m9d.sroom.common.repository.review.ReviewJpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class CourseRemover {
@@ -19,11 +24,14 @@ public class CourseRemover {
 
     private final LectureJpaRepository lectureRepository;
 
-    public CourseRemover(CourseJpaRepository courseRepository, CourseVideoJpaRepository courseVideoRepository, CourseQuizJpaRepository courseQuizRepository, LectureJpaRepository lectureRepository) {
+    private final ReviewJpaRepository reviewRepository;
+
+    public CourseRemover(CourseJpaRepository courseRepository, CourseVideoJpaRepository courseVideoRepository, CourseQuizJpaRepository courseQuizRepository, LectureJpaRepository lectureRepository, ReviewJpaRepository reviewRepository) {
         this.courseRepository = courseRepository;
         this.courseVideoRepository = courseVideoRepository;
         this.courseQuizRepository = courseQuizRepository;
         this.lectureRepository = lectureRepository;
+        this.reviewRepository = reviewRepository;
     }
 
     @Transactional
@@ -31,8 +39,17 @@ public class CourseRemover {
         courseQuizRepository.deleteByCourseId(courseEntity.getCourseId());
         courseVideoRepository.deleteByCourseId(courseEntity.getCourseId());
 
+        List<LectureEntity> lectureEntityList = lectureRepository.findListByCourseId(courseEntity.getCourseId());
+        for(LectureEntity lectureEntity : lectureEntityList){
+            makeLectureIdNull(lectureEntity);
+        }
+
         lectureRepository.deleteByCourseId(courseEntity.getCourseId());
         courseEntity.getMember().getCourses().remove(courseEntity);
         courseRepository.deleteById(courseEntity.getCourseId());
+    }
+
+    private void makeLectureIdNull(LectureEntity lectureEntity) {
+        lectureEntity.getReview().removeLecture();
     }
 }
